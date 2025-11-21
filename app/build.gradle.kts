9fdb2bec41515f3f5866990320fa11d4992dbc54
Samson Momanyi <samsonmomanyi2005@gmail.com>
2025-11-21 13:07:05 +0300

    TimerScreenBottomButtons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("androidx.compose.material:material-icons-extended:1.7.2")
+    implementation(libs.androidx.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -74,7 +74,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation("androidx.compose.material3:material3:1.3.1") // or latest
+    implementation( "androidx.compose.material3:material3:1.2.0")
+    //implementation("androidx.compose.material3:material3:1.3.1") // or latest
     //hilt
     implementation(libs.hilt.android)
 //ksp for hilt
--- app/src/main/java/com/example/chess_clock/ui/TimerSelection.kt
+++ app/src/main/java/com/example/chess_clock/ui/TimerSelection.kt
@@ -1,36 +1,33 @@
 package com.example.chess_clock.ui
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ButtonElevation
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material.icons.filled.ModeEdit
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
-import androidx.compose.material3.Text
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.chess_clock.ViewModel.clockViewModel
 import com.example.chess_clock.model.database.clocks.ClockFormat
+import com.example.chess_clock.ui.theme.ActiveColorScheme
+import com.example.chess_clock.ui.theme.InactiveColorScheme
 
 
 @Composable
@@ -44,72 +41,82 @@ fun TimerSelection(
         modifier = modifier
     ) {
 
-        Image(
-            painter = painterResource(
-                res.drawable.chess_landscape_image
-            ),
-            contentDescription = "chess landscape",
-            modifier = Modifier.weight(0.2f)
-        )
 
-        LazyColumn (){
+        LazyColumn (
+            modifier = Modifier.weight(0.9F)
+        ){
 
         }
 
-        Box(
-            modifier = Modifier.weight(0.2F),
-            contentAlignment = Alignment.BottomStart,
-
+        Row(
+            modifier = Modifier.weight(0.1F),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
         ) {
+
+//            ExtendedFloatingActionButton(
+//                onClick = { /* do something */ },
+//                text = { Text(text = "Extended FAB") },
+//                icon = { Icon(Icons. Filled. Add, "Localized description") },
+//            )
+
             FloatingActionButton(
-
                 onClick = {
-                },
-                shape = ButtonDefaults.elevatedShape,
-                contentColor = Color.White,
-                containerColor = Color.Yellow
 
-            ) {
+                },
+                modifier = Modifier.weight(0.35F),
+                contentColor = ActiveColorScheme.contentColor,
+                containerColor = ActiveColorScheme.backGroundColor
+            ){
                 Text(
-                    text = "Start"
+                    text = "Start",
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold,
                 )
             }
 
+            Spacer(modifier = Modifier.weight(0.35F))
+
             FloatingActionButton(
 
                 onClick = {
                 },
-                shape = ButtonDefaults.elevatedShape,
-                contentColor = Color.White,
-                containerColor = Color.Blue
+                modifier =  Modifier.weight(0.10f)
+                    .size(60.dp),
+               // shape = ButtonDefaults.elevatedShape,
+                contentColor = InactiveColorScheme.contentColor,
+                containerColor = InactiveColorScheme.backGroundColor
 
             ) {
                 Icon(
                     modifier = Modifier.fillMaxSize(),
-                    imageVector = Icons.Default.Edit,
+                    imageVector = Icons.Default.ModeEdit,
                     contentDescription = "Edit Time"
                 )
             }
 
+            Spacer(modifier = Modifier.weight(0.05F))
 
             FloatingActionButton(
 
                 onClick = {
                 },
-                shape = ButtonDefaults.elevatedShape,
-                contentColor = Color.White,
-                containerColor = Color.Blue
-
+                modifier =  Modifier.weight(0.10f)
+                    .size(60.dp),
+                contentColor = InactiveColorScheme.contentColor,
+                containerColor = InactiveColorScheme.backGroundColor
             ) {
                 Icon(
                     modifier = Modifier.fillMaxSize(),
-                    imageVector = Icons.Default.Edit,
+                    imageVector = Icons.Default.Add,
                     contentDescription = "Edit Time"
                 )
             }
          }
     }
 }
+
+
 
 @Composable
 fun TimerCard(clockFormat : ClockFormat){
